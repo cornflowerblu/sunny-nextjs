3de4370c2c2be3ae8650259af902395a7e1c1d05
Roger Urich <rogerdurich+git@gmail.com>
2022-10-14 04:38:27 -0700

    added layout even though react is mad

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-function HomePage() {
-    return <div>Welcome to Next.js!</div>
+import Layout from "../components/layout"
+
+export default function Index() {
+    return (
+        <section>
+            <h2>Layout Example (Index)</h2>
+            <p>
+                This example adds a property <code>getLayout</code> to your page,
+                allowing you to return a React component for the layout. This allows you
+                to define the layout on a per-page basis. Since we're returning a
+                function, we can have complex nested layouts if desired.
+            </p>
+        </section>
+    )
 }
 
-export default HomePage
+Index.getLayout = function getLayout(page) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    )
+}
