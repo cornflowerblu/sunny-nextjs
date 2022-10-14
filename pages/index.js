42f5a248d6f28ba0dec33c4fe07d6bb401903af7
Roger Urich <rogerdurich+git@gmail.com>
2022-10-14 05:31:38 -0700

    interesting....
    doesn't seem to work with cookies tho

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,16 @@ import Layout from "../components/layout"
 export default function Index({ data }) {
     return (
         <>
-            <div class="mx-auto text-center">
-                <h1 class="display-6 pb-2">{data.title}</h1>
+            <div className="mx-auto text-center">
+                <h1 className="display-6 pb-2">{data.title}</h1>
 
-                <div class="d-flex align-items-center justify-content-center pb-2">
+                <div className="d-flex align-items-center justify-content-center pb-2">
                     <img src={data.image} alt="It's Always Sunny in Philadelphia Cast Member" />
                 </div>
-                <div class="recommendation">
-                    <p class="fs-5 text-primary shadow p-3 mt-3 bg-body rounded">{data.name} says you should watch <br /> Season {data.season}, Episode {data.episode}.</p>
-                    <div class="d-flex flex-row" style={{ marginBottom: "3rem" }}><a class="btn btn-primary btn-lg w-50 mt-3 me-2" href="/">Shuffle</a>
-                        <a class="btn btn-outline-primary btn-lg w-50 mt-3 ms-2" href="/details">Details</a>
+                <div className="recommendation">
+                    <p className="fs-5 text-primary shadow p-3 mt-3 bg-body rounded">{data.name} says you should watch <br /> Season {data.season}, Episode {data.episode}.</p>
+                    <div className="d-flex flex-row" style={{ marginBottom: "3rem" }}><a className="btn btn-primary btn-lg w-50 mt-3 me-2" href="/">Shuffle</a>
+                        <a className="btn btn-outline-primary btn-lg w-50 mt-3 ms-2" href="/details">Details</a>
                     </div>
                 </div>
             </div>
