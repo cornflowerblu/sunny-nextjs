6fd582814acbe829c62a332ae340bb399ae0d742
Roger Urich <rogerdurich+git@gmail.com>
2022-10-14 11:39:03 -0700

    details working and can now be hidden

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,62 @@
+import React from "react";
 import Layout from "../components/layout"
 import { useCookies } from 'react-cookie'
-import { NextResponse } from 'next/server'
 
-export default function Index({ data, cookie }) {
+export default function Index({ epData, detData, cookie }) {
     const [cookies, setCookie, removeCookie] = useCookies(['_sunnysession']);
-
     setCookie(cookie)
+
+    const [showDetails, setShowDetails] = React.useState(false)
+    const onClick = () => showDetails ? setShowDetails(false) : setShowDetails(true)
+
+    const Details = () => {
+        return (
+            <div>
+                <h3 className="display-6 pb-2">{detData.title}</h3>
+                <p className="lead fs-4">{detData.description}</p>
+            </div>
+        )
+    }
 
     return (
         <>
             <div className="mx-auto text-center">
-                <h1 className="display-6 pb-2">{data.title}</h1>
-
+                <h1 className="display-6 pb-2">{epData.title}</h1>
                 <div className="d-flex align-items-center justify-content-center pb-2">
-                    <img src={data.image} alt="It's Always Sunny in Philadelphia Cast Member" />
+                    <img src={epData.image} alt="It's Always Sunny in Philadelphia Cast Member" />
                 </div>
                 <div className="recommendation">
-                    <p className="fs-5 text-primary shadow p-3 mt-3 bg-body rounded">{data.name} says you should watch <br /> Season {data.season}, Episode {data.episode}.</p>
+                    <p className="fs-5 text-primary shadow p-3 mt-3 bg-body rounded">{epData.name} says you should watch <br /> Season {epData.season}, Episode {epData.episode}.</p>
                     <div className="d-flex flex-row" style={{ marginBottom: "3rem" }}><a className="btn btn-primary btn-lg w-50 mt-3 me-2" href="/">Shuffle</a>
-                        <a className="btn btn-outline-primary btn-lg w-50 mt-3 ms-2" href="/details">Details</a>
+                        <a className="btn btn-outline-primary btn-lg w-50 mt-3 ms-2" onClick={onClick}>Details</a>
                     </div>
+                    {showDetails ? <Details /> : null}
                 </div>
             </div>
         </>
     )
 }
 
+
+
 export async function getServerSideProps({ req }) {
-    const res = await fetch('http://localhost:3000', {
+    const epRes = await fetch('http://localhost:3000', {
         headers: {
             cookie: req.headers.cookie
         }
     })
-    const data = await res.json()
-    const cookie = res.headers.get('set-cookie')
+    const epData = await epRes.json()
+    const cookie = epRes.headers.get('set-cookie')
 
-    return { props: { data, cookie } }
+    const detRes = await fetch('http://localhost:3000/details', {
+        headers: {
+            cookie: req.headers.cookie
+        }
+    })
+
+    const detData = await detRes.json()
+
+    return { props: { epData, detData, cookie } }
 }
 
 Index.getLayout = function getLayout(page) {
