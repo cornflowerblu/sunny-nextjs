ad6ce566d6409ab5671e86eb0ae32ecafdaa42ae
Roger Urich <rogerdurich+git@gmail.com>
2022-10-18 21:02:35 -0700

    revalidate true

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export async function getStaticProps({ req }) {
     const epData = await epRes.json()
     const cookie = epRes.headers.get('set-cookie')
 
-    return { props: { epData, cookie } }
+    return { props: { epData, cookie }, revalidate: true }
 }
 
 Index.getLayout = function getLayout(page) {
