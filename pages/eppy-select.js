85e6dd1226500caa433efc37b3262a5ba9e85fb5
Roger Urich <rogerdurich+git@gmail.com>
2022-10-21 10:26:24 -0700

    almost working!

--- pages/eppy-select.js
+++ pages/eppy-select.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from "../components/layout"
 
-export default function SelectEpisode() {
+export default function SelectEpisode({shows}) {
+
+console.log(shows)
 
 const [ episodeId, setEpisodeId ] = React.useState(false)
 const Episode = () => {
@@ -15,7 +17,7 @@ const Episode = () => {
           name="id"
           id="id"
           value=''
-          readonly
+          readOnly
           disabled
           style={{cursor: "notAllowed"}}
         ></input>
@@ -24,29 +26,40 @@ const Episode = () => {
 }
 const showEpisode = () => setEpisodeId(true)
 
+const [show, setShow] = React.useState();
+const [season, setSeason] = React.useState();
+
+
 return(
   <div className="container-fluid">
     <div className="row pb-4">
       <h1 className="display-6 fs-1"> Pick an Episode to Edit</h1>
       <div className="col">
         <form id="selectShow" method="POST">
-          <select id="showPicker" class="form-select w-100">
+          <select id="showPicker" className="form-select w-100" onChange={e => setShow(e.currentTarget.value)}>
             <option value=""> Select a Show </option>
+            <option value="950e38a3-3242-44dc-8585-fd30ced6627e"> It's Always Sunny in Philadelphia </option>
           </select>
         </form>
       </div>
       <div className="col">
         <form id="selectSeason" method="POST">
-          <select id="seasonPicker" className="form-select w-100">
+          <select id="seasonPicker" className="form-select w-100" onChange={e => setSeason(e.currentTarget.value)}>
             <option value=""> Select a show to see seasons </option>
+            {show ? shows.seasons_aggregate.nodes.map((season) => (
+                <option key={season.id} value={season.id}>Season: {season.season_number}</option>
+                
+            )):null}
           </select>
         </form>
       </div>
       <div className="col">
         <form id="selectEpisode" method="POST">
-          <select id="episodePicker" class="form-select w-100" onChange={showEpisode}>
+          <select id="episodePicker" className="form-select w-100" onChange={showEpisode} >
             <option value=""> Select a show to see episodes</option>
-            <option value="234"> A Change</option>
+            {season ? shows.seasons_aggregate.nodes.filter((seasons_aggregate) => seasons_aggregate.id).map(((episode) => (
+                <option key={episode.id} value={episode.id}>Episode {episode.episode_number}: {episode.title}</option>)))
+            :null}
           </select>
         </form>
       </div>
@@ -54,6 +67,21 @@ return(
     {episodeId ? <Episode /> : null}
   </div>
 )}
+
+export async function getStaticProps() {
+
+    const res = await fetch(process.env.API_URL, {
+        headers: {
+            "x-hasura-admin-secret": process.env.GRAPHQL_SECRET
+        }
+    })
+
+    const rawData = await res.json()
+
+    const shows = rawData.shows_by_pk
+
+    return { props: { shows } , revalidate: 300 }
+}
 
 SelectEpisode.getLayout = function getLayout(page) {
     return (
