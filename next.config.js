bfb271ac9581764dbabce8db58b57a15ced155f1
Roger Urich <rogerdurich+git@gmail.com>
2023-01-12 19:07:30 -0800

    not gonna rewrite the base url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/',
+        source: '/shows',
         destination: '/shows/1',
         permanent: true,
       },
