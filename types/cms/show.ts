84109871ed691c6c7aa25bfddcc5ace4323fbae9
Roger Urich <rogerdurich@gmail.com>
2023-02-10 06:20:51 -0800

    add typescript part 1

---
+++ types/cms/show.ts
@@ -0,0 +1,14 @@
+export interface Show {
+  cms_: any
+  id: number,
+  attributes: ShowAttributes
+}
+
+export interface ShowAttributes {
+  name: string,
+  created_at: string,
+  updated_at: string,
+  published_at: string,
+  slug: string,
+  short_name: string,
+}
