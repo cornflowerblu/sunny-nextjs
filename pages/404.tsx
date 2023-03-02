ab4489263aecdca15b642edbe8e7c6a884be1b81
Roger Urich <rogerdurich@gmail.com>
2023-03-02 10:22:49 -0800

    linter fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,7 +10,7 @@ const NotFoundPage = () => {
   return (
     <div className={styles.container}>
       <h1 className="fw-light">Whoops!</h1>
-      <p className="lead text-muted">Looks like we couldn't find what you were looking for...</p>
+      <p className="lead text-muted">Looks like we couldn&apos;t find what you were looking for...</p>
       <Image src="/404-error-page.jpg" alt="404" width={598} height={398} style={{ marginBottom: '-50px' }} />
       <GoHomeFooter />
     </div>
